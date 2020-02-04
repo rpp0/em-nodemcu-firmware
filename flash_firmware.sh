f7fb7be60f02298a8c2dffb108c6305645720065
Mariano Di Martino <mariano@mariano.in.edm.uhasselt.be>
2020-02-04 16:14:25 +0100

    Added OpenSSL AES ECB

--- flash_firmware.sh
+++ flash_firmware.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-esptool -b 115200 --port /dev/ttyUSB0 write_flash -fm dio 0x00000 ./bin/0x00000.bin
-esptool -b 115200 --port /dev/ttyUSB0 write_flash -fm dio 0x10000 ./bin/0x10000.bin
+esptool.py -b 115200 --port /dev/ttyUSB0 write_flash -fm dio 0x00000 ./bin/0x00000.bin
+esptool.py -b 115200 --port /dev/ttyUSB0 write_flash -fm dio 0x10000 ./bin/0x10000.bin
